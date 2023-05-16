63568478f1173f402fa6a5dad910133d36a29f61
SoukaynaFR <elferchounisoukayna@gmail.com.com>
2023-05-16 16:27:37 +0100

    login v1

---
+++ login/src/main.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.css"
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import "bootstrap/dist/js/bootstrap.min.js"
+
+createApp(App).use(router).mount('#app')
+
+
