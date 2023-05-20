25f525e9183279fc5139cdf6f50d328cefe8fe19
lakramat hasnae <hasnaelakramat@etu.uae.ac.ma>
2023-05-20 15:01:42 +0100

    reference_migration

---
+++ laravel/Integration/app/Http/Controllers/PaiementController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePaiementRequest;
+use App\Http\Requests\UpdatePaiementRequest;
+use App\Models\Paiement;
+
+class PaiementController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePaiementRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Paiement $paiement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Paiement $paiement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePaiementRequest $request, Paiement $paiement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Paiement $paiement)
+    {
+        //
+    }
+}
