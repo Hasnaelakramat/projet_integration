460b4270833579d3e063608698e4479bb8e2752c
lakramat hasnae <hasnaelakramat@etu.uae.ac.ma>
2023-06-12 17:07:15 +0100

    front+back

--- Version+mail/storage/framework/views/8f882c12836e32c01c5d0dee3588b2e1.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH C:\Users\PE\Downloads\your-project-name\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
