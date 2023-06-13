b6586501aa08a3349a852b0677d77d8ec590f026
lakramat hasnae <hasnaelakramat@etu.uae.ac.ma>
2023-06-13 14:58:28 +0100

    ajout d'intervention + les fonction de paiment +chiffrage+linkage

--- Version+mail/storage/framework/views/fda4f19fb4f2af5233fce5ad78a2ba37.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH C:\xampp\htdocs\course_project\your-project-name\vendor\laravel\framework\src\Illuminate\Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
